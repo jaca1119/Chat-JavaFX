f0f269cd27f4551f6350f864885af385c7436bb2
jaca1119 <laki1663@gmail.com>
2019-12-23 21:30:55 +0100

    Testing: set text, get text

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,18 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
 
 
 public class Controller
 {
     @FXML
     private Label labelText;
+    @FXML
+    private TextField textField;
+
 
     public void addText()
     {
@@ -15,4 +21,18 @@ public class Controller
         labelText.setText("Button clicked " + Math.random());
     }
 
+    public void setText()
+    {
+        String textFieldText = textField.getText();
+
+        if (Objects.equals(textFieldText, ""))
+        {
+            labelText.setText("Text field is empty!");
+        }
+        else
+        {
+            labelText.setText(textFieldText);
+            textField.setText("");
+        }
+    }
 }
