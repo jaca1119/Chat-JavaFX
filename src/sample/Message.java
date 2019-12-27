d726841501a5535a021c483d1bb2f36dc55fed61
jaca1119 <laki1663@gmail.com>
2019-12-27 11:17:25 +0100

    Byte array for image

--- src/sample/Message.java
+++ src/sample/Message.java
@@ -1,22 +1,32 @@
 package sample;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class Message implements Serializable
 {
     private final MessageType messageType;
     private final String data;
-
+    private final byte[] image;
     public Message(MessageType messageType)
     {
         this.messageType = messageType;
         data = null;
+        image = new byte[0];
     }
 
     public Message(MessageType messageType, String data)
     {
         this.messageType = messageType;
         this.data = data;
+        this.image = new byte[0];
+    }
+
+    public Message(MessageType messageType,final byte[] image)
+    {
+        this.messageType = messageType;
+        this.image = image;
+        this.data = null;
     }
 
     public MessageType getMessageType()
@@ -27,5 +37,10 @@ public class Message implements Serializable
     public String getData()
     {
         return data;
+    }
+
+    public byte[] getImage()
+    {
+        return image;
     }
 }
